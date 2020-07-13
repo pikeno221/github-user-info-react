394611414a7294c10ff9ab00d6443ee71e4322a9
pikeno221 <gabriellgomesbh@gmail.com>
2020-07-12 22:52:08 -0300

    adding styled-component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import { } from './styles';
+
 const Dashboard: React.FC = () => {
 
-  return <h1> Dashboard </h1>
+  return <h1> Explore repositórios no GitHub </h1>
 
 };
 
